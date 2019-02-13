846c80e7274b4a047f0ea578eba4c268505bbd75
Fabiano Voss Rieg <fabiano.voss@linx.com.br>
2019-02-13 21:15:10 -0200

    Atualização sistema-c.

--- sistema-c/app/app.py
+++ sistema-c/app/app.py
@@ -10,7 +10,6 @@ api = Api(app)
 
 app.config['SECRET_KEY'] = '0fec2613-6739-42ce-ac64-21bc53898d1d'
 app.config['JWT_SECRET_KEY'] = '660e0bda-e8de-476e-8165-2a94f9a5572c'
-#app.config['JWT_BLACKLIST_ENABLED'] = True
 app.config['JWT_BLACKLIST_TOKEN_CHECKS'] = ['access', 'refresh']
 
 jwt = JWTManager(app)
@@ -20,4 +19,4 @@ api.add_resource(EventoCPF, '/cpf/evento')
 
 
 if __name__ == '__main__':
-	app.run('0.0.0.0', port=5003, debug=True)
+	app.run(host='0.0.0.0')
