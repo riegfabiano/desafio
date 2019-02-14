893045d8ada50b45bc8701b96488cb3b3271e0e7
Fabiano Voss Rieg <fabiano.voss@linx.com.br>
2019-02-13 23:03:11 -0200

    Alteração no sistema graphql.

--- graphql-server/app/app.py
+++ graphql-server/app/app.py
@@ -2,7 +2,7 @@ import requests
 import os
 from flask import Flask
 from flask_graphql import GraphQLView
-from graphene import (String, Int, Float, DateTime, List, Schema, Field, ObjectType)
+from graphene import (String, Int, Float, List, Schema, Field, ObjectType)
 
 
 app = Flask(__name__)
@@ -49,7 +49,7 @@ class ScoreCPF(ObjectType):
 	idade = Int()
 	bemList = List(Bem)
 	endereco = Field(Endereco)
-	fonte_renda = FonteRenda
+	fonteRenda = Field(FonteRenda)
 
 
 # --- Evento
@@ -60,12 +60,12 @@ class MovtoFinanceiro(ObjectType):
 
 
 class UltimaCompraCartao(ObjectType):
-	data = DateTime()
+	data = String()
 	valor = Float()
 
 
 class EventoCPF(ObjectType):
-	ultimaConsulta = DateTime()
+	ultimaConsulta = String()
 	movtoFinanceiroList = List(MovtoFinanceiro)
 	ultimaCompraCartao = Field(UltimaCompraCartao)
 
@@ -78,67 +78,114 @@ class ConsultaCPF(ObjectType):
 
 
 class Query(ObjectType):
-	login = String(
-		username=String(required=True),
-		password=String(required=True)
-	)
 	consultaCPF = Field(
 		ConsultaCPF,
 		cpf=String(required=True),
-		access_token=String(required=True)
+		username=String(required=True),
+		password=String(required=True)
 	)
 
-	def resolve_login(self, info, username, password):
-		return call_ws_login(os.getenv('HOST_A', 'localhost'), username, password)
-
-	def resolve_consultaCPF(self, info, cpf, access_token):
+	def resolve_consultaCPF(self, info, cpf, username, password):
 		return ConsultaCPF(
-			self.__make_situacao(cpf, access_token),
-			self.__make_score(cpf, access_token),
-			self.__make_evento(cpf, access_token)
+			make_situacao(cpf, username, password),
+			make_score(cpf, username, password),
+			make_evento(cpf, username, password)
 		)
 
-	def __make_situacao(self, cpf, access_token):
-		result = call_ws(os.getenv('HOST_A', 'localhost'), cpf, access_token)
 
-		E = Endereco(
-			logradouro=result['endereco']['logradouro'],
-			numero=result['endereco']['numero'],
-			bairro=result['endereco']['bairro'],
-			cidade=result['endereco']['cidade'],
-			uf=result['endereco']['uf'],
-			cep=result['endereco']['cep']
-		)
-		divida_list = []
+def make_situacao(cpf, username, password):
+	access_token = call_ws_login(os.getenv('HOST_A', 'localhost'), username, password)
+	result = call_ws(os.getenv('HOST_A', 'localhost'), "cpf/situacao", cpf, access_token)
 
-		for divida_dict in result['divida_list']:
-			divida_list.append(
-				Divida(
-					credorNome=divida_dict['credor_nome'],
-					credorCnpj=divida_dict['credor_cnpj'],
-					nrContrato=divida_dict['nr_contrato'],
-					valor=divida_dict['valor']
-				)
+	E = Endereco(
+		logradouro=result['endereco']['logradouro'],
+		numero=result['endereco']['numero'],
+		bairro=result['endereco']['bairro'],
+		cidade=result['endereco']['cidade'],
+		uf=result['endereco']['uf'],
+		cep=result['endereco']['cep']
+	)
+	divida_list = []
+
+	for divida_dict in result['divida_list']:
+		divida_list.append(
+			Divida(
+				credorNome=divida_dict['credor_nome'],
+				credorCnpj=divida_dict['credor_cnpj'],
+				nrContrato=divida_dict['nr_contrato'],
+				valor=divida_dict['valor']
 			)
-
-		return SituacaoCPF(
-			nome=result['nome'],
-			cpf=cpf,
-			endereco=E,
-			dividaList=divida_list
 		)
 
-	def __make_score(self, cpf, access_token):
-		pass
+	return SituacaoCPF(
+		nome=result['nome'],
+		cpf=cpf,
+		endereco=E,
+		dividaList=divida_list
+	)
 
-	def __make_evento(self, cpf, access_token):
-		pass
 
-	# def resolve_score(self, info):
-	# 	return call_ws(os.getenv('HOST_B', 'localhost'), info)
+def make_score(cpf, username, password):
+	host = os.getenv('HOST_B', 'localhost')
+	access_token = call_ws_login(host, username, password)
+	result = call_ws(host, "cpf/score", cpf, access_token)
 
-	# def resolve_evento(self, info):
-	# 	return call_ws(os.getenv('HOST_C', 'localhost'), info)
+	E = Endereco(
+		logradouro=result['endereco']['logradouro'],
+		numero=result['endereco']['numero'],
+		bairro=result['endereco']['bairro'],
+		cidade=result['endereco']['cidade'],
+		uf=result['endereco']['uf'],
+		cep=result['endereco']['cep']
+	)
+	FR = FonteRenda(
+		cnpj=result['fonte_renda']['cnpj'],
+		razaoSocial=result['fonte_renda']['razao_social'],
+		salario=result['fonte_renda']['salario']
+	)
+	bem_list = []
+
+	for bem_dict in result['bem_list']:
+		bem_list.append(
+			Bem(
+				descricao=bem_dict['descricao'],
+				valor=bem_dict['valor']
+			)
+		)
+
+	return ScoreCPF(
+		idade=result['idade'],
+		bemList=bem_list,
+		endereco=E,
+		fonteRenda=FR
+	)
+
+
+def make_evento(cpf, username, password):
+	host = os.getenv('HOST_C', 'localhost')
+	access_token = call_ws_login(host, username, password)
+	result = call_ws(host, "cpf/evento", cpf, access_token)
+
+	UCC = UltimaCompraCartao(
+		data=result['ultima_compra_cartao']['data'],
+		valor=result['ultima_compra_cartao']['valor']
+	)
+	movto_list = []
+
+	for movto_dict in result['movto_financeiro_list']:
+		movto_list.append(
+			MovtoFinanceiro(
+				tipo=movto_dict['tipo'],
+				valor=movto_dict['valor'],
+				instituicao=movto_dict['instituicao']
+			)
+		)
+
+	return EventoCPF(
+		ultimaConsulta=result['ultima_consulta'],
+		movtoFinanceiroList=movto_list,
+		ultimaCompraCartao=UCC
+	)
 
 
 def call_ws_login(host, username, password):
@@ -146,7 +193,7 @@ def call_ws_login(host, username, password):
 	Obtem Token
 	"""
 
-	url = "http://%s:5001/login" % os.getenv('HOST_A', 'localhost')
+	url = "http://%s:5000/login" % host
 	headers = {
 		'Content-Type': 'application/json'
 	}
@@ -157,12 +204,12 @@ def call_ws_login(host, username, password):
 	return requests.post(url, headers=headers, json=data).json()['access_token']
 
 
-def call_ws(host, cpf, access_token):
+def call_ws(host, urn, cpf, access_token):
 	"""
 	Obtem dados
 	"""
 
-	url = "http://%s:5001/cpf/situacao" % os.getenv('HOST_A', 'localhost')
+	url = "http://%s:5000/%s" % (host, urn)
 	headers = {
 		'Content-Type': 'application/json',
 		'Authorization': 'Bearer %s' % access_token
@@ -177,28 +224,6 @@ def call_ws(host, cpf, access_token):
 
 schema = Schema(query=Query)
 
-query = """
-{
-	situacao(cpf: "29922693364") {
-		nome
-		endereco {
-			logradouro
-			numero
-			bairro
-			cidade
-			uf
-			cep
-		}
-		dividaList {
-			credorNome
-			credorCnpj
-			valor
-			nrContrato
-		}
-	}
-}
-"""
-
 # Routes
 app.add_url_rule(
 	'/graphql',
@@ -211,5 +236,4 @@ app.add_url_rule(
 
 
 if __name__ == '__main__':
-	app.run()
-	#print(schema.execute(query).data)
+	app.run(host='0.0.0.0')
